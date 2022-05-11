bee50344e41d10f9e5772cd46ef8d89f498b98da
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2022-05-10 19:36:08 -0700

    add metamask install check and flow when not installed

--- start/contract.js
+++ start/contract.js
@@ -30,9 +30,42 @@ The `piggybankContract` is compiled from:
   }
 */
 
-const forwarderOrigin = 'http://localhost:9010'
+const forwarderOrigin = "http://localhost:9010";
 
 const initialize = () => {
-  //You will start here 
-}
-window.addEventListener('DOMContentLoaded', initialize)
+  const onboardButton = document.getElementById("connectButton");
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetamaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  // using @metamask/onboarding' library => https://github.com/MetaMask/metamask-onboarding#metamask-onboarding
+  //We create a new MetaMask onboarding object to use in our app
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+  //This will start the onboarding proccess
+  const onClickInstall = () => {
+    onboardButton.innerText = "Onboarding in progress";
+    onboardButton.disabled = true;
+    //On this object we have startOnboarding which will start the onboarding process for our end user
+    onboarding.startOnboarding();
+  };
+
+  const MetaMaskClientCheck = () => {
+    if (!isMetamaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = "Click here to install MetaMask!";
+      //When the button is clicked we call onClickInstall
+      onboardButton.onclick = onClickInstall;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    } else {
+      onboardButton.innerText = "Connect";
+    }
+  };
+  MetaMaskClientCheck();
+};
+window.addEventListener("DOMContentLoaded", initialize);
